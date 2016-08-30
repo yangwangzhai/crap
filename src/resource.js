3732c7b6165522c1f328e29d0001b19500b28433
ywz <312778836@qq.com>
2016-08-30 09:22:02 +0800

    0830-0921

---
+++ src/resource.js
@@ -0,0 +1,30 @@
+var res = {
+    S_bg : "res/background.png",
+    s_1to6 : "res/1to6.png",
+    s_bottom_bg : "res/bottom_bg.png",
+    s_craps_plist : "res/craps.plist",
+    s_craps_png : "res/craps.png",
+    s_count1 : "res/count1.png",
+    s_count2 : "res/count2.png",
+    s_count3 : "res/count3.png",
+    s_count4 : "res/count4.png",
+    s_count5 : "res/count5.png",
+    s_count6 : "res/count6.png",
+    s_countbig : "res/countbig.png",
+    s_countsmall : "res/countsmall.png",
+    s_countsingle : "res/countsingle.png",
+    s_countdouble : "res/countdouble.png",
+    s_bet5 : "res/bet5.png",
+    s_bet10 : "res/bet10.png",
+    s_bet20 : "res/bet20.png",
+    s_bet50 : "res/bet50.png",
+    s_bet100 : "res/bet100.png",
+
+    s_bg_music : "res/audio/bg_music.mp3",
+    s_yao : "res/audio/yao1.mp3"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
